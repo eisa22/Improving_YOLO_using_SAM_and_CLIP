52b32dac3f275d8abd49d2c4b98762ec8766be69
eisa22 <einspieler.work@gmail.com>
2024-04-27 12:43:06 +0200

    Image patchin implementation

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import YOLO
 
-
-
 if __name__ == "__main__":
     # Control Panel
     # Choose camera idx: 0 for laptop webcam; idx:1 for external camera
     camera_idx = 1
 
     # Choose if webcam should be active (True) or image should be used (False)
-    webcam = True
+    webcam = False
 
     # Image path
-    image_path = 'Images/Table_with_objects.jpg'
+    image_path = 'Images/subimage3.jpg'
 
     detector = YOLO.ObjectDetection(camera_idx, webcam, image_path)
     detector()
