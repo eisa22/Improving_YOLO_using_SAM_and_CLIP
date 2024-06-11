ee03fa495714253beeb36c4965fafb66d16b793d
eisa22 <einspieler.work@gmail.com>
2024-06-11 13:10:45 +0200

    Correct yolo labels are put

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import YOLO
+import YOLO_V8
 import Helper_Functions
 import Validation
 import OpenAI_Key
@@ -7,6 +7,7 @@ from pycocotools.coco import COCO
 import requests
 from PIL import Image, ImageDraw
 from io import BytesIO
+import numpy as np
 
 
 # Control Panel
@@ -46,22 +47,27 @@ def draw_bounding_boxes(bbox_list, image_url):
 
 if __name__ == "__main__":
 
-    detector = YOLO.ObjectDetection(camera_idx, webcam, debug_mode, conf_threshold)
-    helper = Helper_Functions.Helper_Functions()
-    validation = Validation.Validation()
-
+    yolo = YOLO_V8.YOLO_V8('yolov8n.pt')  # Initialize the YOLO_V8 class with the model path
     image_ids = coco_gt.getImgIds()
     all_ious = []
 
-    for image_id in image_ids:
-
-        img_info = coco_gt.loadImgs(image_id)[0]
-        print("Processing image: ", img_info['file_name'])
-        coco_url = img_info['coco_url']
-        YoloData, confidences, image_url, frame = detector.get_YOLO_Results(coco_url, image_id)
-        print("YoloData: ", YoloData)
-        # validation.calculate_IoU(YoloData, coco_gt, coco_url, debug_mode)
-        draw_bounding_boxes(YoloData, image_url)
-    validation.calculate_average_iou()
-
-
+    with open("Results_IoU.txt", "w") as f:
+        for image_id in image_ids:
+            img_info = coco_gt.loadImgs(image_id)[0]
+            print("Processing image: ", img_info['file_name'])
+            f.write(f"Processing image: {img_info['file_name']}\n")
+            image_url = img_info['coco_url']
+            image, pred_boxes, pred_class_ids = yolo.get_detections(image_url)
+            # Get ground truth annotations
+            ann_ids = coco_gt.getAnnIds(imgIds=image_id)
+            anns = coco_gt.loadAnns(ann_ids)
+            gt_boxes = []
+            gt_class_ids = []
+            for ann in anns:
+                bbox = ann['bbox']
+                # Convert bbox format from [x, y, width, height] to [xmin, ymin, xmax, ymax]
+                gt_boxes.append([bbox[0], bbox[1], bbox[0] + bbox[2], bbox[1] + bbox[3]])
+                gt_class_ids.append(ann['category_id'])
+            yolo.plot_detections(image, pred_boxes, pred_class_ids, gt_boxes, gt_class_ids)
+            # Calculate IoU for matched bounding boxes by class names
+            matched_ious = []
